0761b79cb1d1c8f3121d1e457e926f355b7c328a
ecanvasser <eddiecanvassere9@gmail.com>
2022-12-05 19:06:03 -0800

    Add global weather object for data storage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import "./style.css";
 
 const Weather = () => {
-  const getData = async () => {
-    console.log("test");
+  var weatherObj = {};
+
+  const getData = async (city) => {
+    for (key in weatherObj) {
+      delete weatherObj[key];
+    }
+
+    let response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=6461858adafc20c62e982c70265b6260&units=imperial`
+    );
+    let data = await response.json();
+    weatherObj["temp"] = data.main.temp;
+    console.log(weatherObj);
   };
 
   return { getData };
