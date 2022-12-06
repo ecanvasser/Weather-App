811907d73558503b3daa5257c8d2db1433831898
ecanvasser <eddiecanvassere9@gmail.com>
2022-12-06 00:39:42 -0800

    Change color schema and add search bar icon

--- src/index.js
+++ src/index.js
@@ -22,35 +22,37 @@ const Weather = () => {
 };
 
 const Display = () => {
-    const setTitle = (name) => {
-        document.getElementById('city').textContent = name;
-    };
+  const setTitle = (name) => {
+    document.getElementById("city").textContent = name;
+  };
 
-    const setStats = (obj) => {
-        document.getElementById('temp').textContent = obj['temp'];
-        document.getElementById('minmax').textContent = `${obj["temp_min"]} / ${obj['temp_max']}`;
-        document.getElementById('vis').textContent = obj['visible'];
-    }
+  const setStats = (obj) => {
+    document.getElementById("temp").textContent = obj["temp"];
+    document.getElementById(
+      "minmax"
+    ).textContent = `${obj["temp_min"]} / ${obj["temp_max"]}`;
+    document.getElementById("vis").textContent = obj["visible"];
+  };
 
-    const build = (name, obj) => {
-        setTitle(name);
-        setStats(obj);
-    }
+  const build = (name, obj) => {
+    setTitle(name);
+    setStats(obj);
+  };
 
-    return { build };
+  return { build };
 };
 
 const checkWeather = async () => {
-    let city = document.getElementById('search').value;
-    let display = Display();
-    let weather = Weather();
+  let city = document.getElementById("search").value;
+  let display = Display();
+  let weather = Weather();
 
-    let pullWeather = await weather.getData(city);
-    let setPage = await display.build(city, weatherObj);
-    console.log(weatherObj);
-}
+  let pullWeather = await weather.getData(city);
+  let setPage = await display.build(city, weatherObj);
+  console.log(weatherObj);
+};
 
-document.getElementById('submit').addEventListener('click', (e) => {
-    checkWeather();
-    e.preventDefault();
-})
+document.getElementById("submit").addEventListener("click", (e) => {
+  checkWeather();
+  e.preventDefault();
+});
